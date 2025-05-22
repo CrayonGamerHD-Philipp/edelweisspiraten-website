f6a6a13d20ec7d2515fb6779c650d311fd991f27
CrayonGamerHD-Philipp <philipp@philipp-hethey.de>
2025-05-22 14:27:08 +0200

    Added Imprint

---
+++ web/src/routes/impressum/+page.svelte
@@ -0,0 +1,14 @@
+<section class="max-w-5xl mx-auto py-12 px-6">
+    <h2>Impressum</h2>
+    <h2 id="m46">Diensteanbieter</h2>
+    <p>Philipp Hethey</p>
+    <p>Rockwinkeler Heerstraße 140</p>
+    <p>28355 Bremen</p>
+
+    <h2 id="m56">Kontaktmöglichkeiten</h2><p>E-Mail-Adresse: info@hethey-bremen.de</p>
+
+    <h2 id="m172">Social Media und andere Onlinepräsenzen</h2><p>Dieses Impressum gilt auch für die folgenden Social-Media-Präsenzen und Onlineprofile: </p>
+    <p><a href="https://www.instagram.com/edelweisspiraten_cpd/" target="_blank">https://www.instagram.com/edelweisspiraten_cpd/</a></p>
+
+    <p class="seal"><a href="https://datenschutz-generator.de/" title="Rechtstext von Dr. Schwenke - für weitere Informationen bitte anklicken." target="_blank" rel="noopener noreferrer nofollow">Erstellt mit kostenlosem Datenschutz-Generator.de von Dr. Thomas Schwenke</a></p>
+</section>
