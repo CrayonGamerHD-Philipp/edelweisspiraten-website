5b1faf6fdd8d0c6fbe930fb63f1cd6a001b18a37
CrayonGamerHD-Philipp <philipp@philipp-hethey.de>
2025-05-22 13:26:15 +0200

    Edit svelte.config.js

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter_static from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,14 +12,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: undefined,
-			precompress: true
-		}),
-		paths: {
-			relative: false
-		}
+		adapter: adapter()
 	}
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: '', // leer bei eigener Domain
+		}
+	}
+};
