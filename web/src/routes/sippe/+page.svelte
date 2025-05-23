948dd0ee21f3849fb2bb86fd44a102f7dedf7a04
CrayonGamerHD-Philipp <philipp@philipp-hethey.de>
2025-05-23 11:33:48 +0200

    Changed Day for Sippe

--- web/src/routes/sippe/+page.svelte
+++ web/src/routes/sippe/+page.svelte
@@ -15,7 +15,7 @@
         <p class="text-gray-700 mt-2">Gruppenleiter</p>
         <p class="text-sm text-gray-500">Melina und Sebastian</p>
         <p class="text-gray-700 mt-2">Urzeit</p>
-        <p class="text-sm text-gray-500">Freitags 16:30 Uhr - 18:00 Uhr</p>
+        <p class="text-sm text-gray-500">Montags 16:30 Uhr - 18:00 Uhr</p>
         <p class="text-gray-700 mt-2">Ort</p>
         <p class="text-sm text-gray-500">Ev. Auferstehungsgemeinde Bremen-Hastedt</p>
         <p class="text-gray-700 mt-2">Alter</p>
