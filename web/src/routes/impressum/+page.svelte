dd749a0318ac043770e5fc43c6f5447f70fd4dd7
CrayonGamerHD-Philipp <philipp@philipp-hethey.de>
2025-12-30 16:21:58 +0100

    Update Impressum

--- web/src/routes/impressum/+page.svelte
+++ web/src/routes/impressum/+page.svelte
@@ -7,12 +7,13 @@
 <section class="max-w-5xl mx-auto py-12 px-6">
     <h2 class="font-bold mb-2">Impressum</h2>
     <h2 id="m46" class="font-bold mb-2">Diensteanbieter</h2>
-    <p>Philipp Hethey</p>
-    <p>Rockwinkeler Heerstraße 140</p>
-    <p class="mb-2">28355 Bremen</p>
+    <p>Stamm Götz von Berlichingen</p>
+    <p>Christliche Pfadfinderschaft Deutschland e.V.</p>
+    <p>Drakenburger Str. 42</p>
+    <p class="mb-2">28207 Bremen</p>
 
     <h2 class="font-bold" id="m56">Kontaktmöglichkeiten</h2>
-    <p class="mb-2">E-Mail-Adresse: <a class="hover:text-blue-600" href="mailto:info@hethey-bremen.de">info@hethey-bremen.de</a></p>
+    <p class="mb-2">E-Mail-Adresse: <a class="hover:text-blue-600" href="mailto:info@edelweisspiaretn-bremen.de">info@edelweisspiaretn-bremen.de</a></p>
 
     <h2 id="m172" class="font-bold">Social Media und andere Onlinepräsenzen</h2>
     <p>Dieses Impressum gilt auch für die folgenden Social-Media-Präsenzen und Onlineprofile: </p>
